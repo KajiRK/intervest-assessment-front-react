2a1bb31a966df6aa57b9e069f94033e56b228c11
KajiRK <kaji.rk@outlook.com>
2023-05-21 18:46:48 +0530

    Update WelcomeCard

--- src/components/Widgets/WelcomeCard.tsx
+++ src/components/Widgets/WelcomeCard.tsx
@@ -1,5 +1,13 @@
 export default function WelcomeCard () {
   return (
-    <div className="rounded-md py-6 px-7.5 bg-teal-400"></div>
+    <div className="rounded-md py-6 px-7.5 bg-teal-400">
+      <div className="flex flex-col lg:flex-row">
+        <div className="pl-6 lg:w-2/3">
+          <h3 className="mb-4 font-semibold">Welcome to your AAPL Ticker Dashboard!</h3>
+          <p className="text-gray-600 text-sm">Lorem Ipsum is simply dummy text of the printing and typesetting industry. <br />Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+        </div>
+        <div className="pl-6 pr-6 order-first lg:order-last">dsdsd</div>
+      </div>
+    </div>
   );
 }
